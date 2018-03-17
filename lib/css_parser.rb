a8a5e627b5bba0686d0f15b78782070342928c96
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-03-17 08:38:48 +0000

    A bit of css parser refactoring

--- lib/css_parser.rb
+++ lib/css_parser.rb
@@ -25,8 +25,7 @@ class CssParser
 
   # 1 or more
   def parse_selectors
-    selectors = []
-    selectors << parse_selector
+    selectors = [parse_selector]
     while next_token_type == :comma
       shift_token(:comma)
       selectors << parse_selector
@@ -35,8 +34,7 @@ class CssParser
   end
 
   def parse_selector
-    raise "Expected word" unless next_token_type == :word
-    selector = []
+    selector = [shift_token(:word)]
     while next_token_type == :word
       selector << shift_token(:word)
     end
@@ -53,18 +51,12 @@ class CssParser
   end
 
   def parse_property
-    raise "Expected word" unless next_token_type == :word
     key = shift_token(:word)
     shift_token(:colon)
-    value = []
-    value << shift_token(:word)
+    value = [shift_token(:word)]
     value << shift_token(:word) while next_token_type == :word
     shift_token(:semicolon)
     CssProperty.new(key: key, value: value)
-  end
-
-  def self.parse(str)
-    new(str).parse
   end
 
   def next_token_type
@@ -75,5 +67,9 @@ class CssParser
   def shift_token(type)
     raise "Expected token #{type}, got #{next_token_type}" unless next_token_type == type
     @tokens.shift.value
+  end
+
+  def self.parse(str)
+    new(str).parse
   end
 end
